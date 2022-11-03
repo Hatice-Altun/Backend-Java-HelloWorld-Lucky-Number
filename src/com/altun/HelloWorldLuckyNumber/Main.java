8be2c77dfc50d9cdb3fdce76c149abbd43e24460
Hatice-Altun <van.altun@outlook.com>
2022-11-03 14:25:06 +0100

    HelloWorldLuckyNumber

---
+++ src/com/altun/HelloWorldLuckyNumber/Main.java
@@ -0,0 +1,28 @@
+package com.altun.HelloWorldLuckyNumber;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// met sout + tab heb je een methode body sneller geschreven
+        System.out.println("Hello World");
+
+        int number = 0;
+
+        System.out.println("Geef invoer op:");
+
+        Scanner userInteraction = new Scanner(System.in);
+        number = userInteraction.nextInt();
+
+        System.out.println("U heeft " + number + " ingevoerd!");
+        System.out.println("Geef naam op:");
+        String userName = userInteraction.next();
+        //cause .next can catch every datatype
+
+        System.out.println("Uw naam is " + userName + " en uw geluksnummer is " + number + ".");
+
+
+
+    }
+}
